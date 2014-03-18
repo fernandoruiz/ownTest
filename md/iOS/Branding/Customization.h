4b2127c2e4258281c20f1b6f6ef6055b5244f3ed
fernandoruiz <fernando.ruiz@thinetic.es>
2014-03-18 12:40:52 +0100

    Brand changes of:   * User: owncloud66 project: md

---
+++ md/iOS/Branding/Customization.h
@@ -0,0 +1,129 @@
+//
+//  Customization.h
+//  ownCloud iOs Client
+//
+//  Created by OWNBRANDER on 2014-03-18 11:39:53 UTC
+//
+//
+
+
+//URLs Setting
+//ios_settings_help_url
+#define k_help_url @"http://www.sofdsdof.es"
+#define k_recomended_url @"http://owncloud.com/mobile/recommend"
+#define k_send_feedback @"http://owncloud.com/mobile/feedback"
+
+//Hide url server
+//ios_settings_server_user
+#define k_hide_url_server YES
+
+//Set a default url server. It must be obligatory if we hide the url server (#define k_hide_url_server YES)
+#define k_default_url_server @"asd"
+
+
+//ios_settings_multiaccount_value
+//Show multiaccount or disconnect
+#define k_multiaccount_available NO
+
+//Appname
+#define k_appname @"md"
+
+
+//Appversion
+#define setting_version @"2013  iOS 3.0.1"
+
+//Have icon on backbutton
+#define k_have_icon_on_popover YES
+
+//Have image background on navigation bar
+#define k_have_image_background_navigation_bar NO
+
+//Have oauth active
+#define k_is_oauth_active NO
+
+//Have SAML active
+#define k_is_sso_active NO
+
+//OAuth server
+#define k_oauth_login @"http://owncloud.tuxed.net/oauth/php-oauth/authorize.php?response_type=code&client_id=com.demo.test.my&scope=&redirect_uri=owncloud://callback"
+#define k_oauth_authorize @"http://owncloud.tuxed.net/oauth/php-oauth/authorize.php"
+#define k_oauth_token @"http://owncloud.tuxed.net/oauth/php-oauth/token.php"
+#define k_oauth_webservice @"http://owncloud.tuxed.net/oauth/php-oauth-grades-rs/api.php/grades/@me"
+#define k_oauth_client_id @"com.demo.test.my" //the same in k_oauth_login!!
+
+//Mail feedback
+//ios_settings_email_address
+#define k_mail_feedback @"asdas@aldads.estitodasLECHES"
+
+
+//Social
+#define k_download_url_short @""
+#define k_download_url_long @""
+#define k_twitter_user @""
+
+
+//Minimun files until show letters on separators
+#define k_minimun_files_to_show_separators 20
+//show logo or name on title of file list
+#define k_show_logo_on_title_file_list YES
+
+//show add more storage to my account
+#define k_show_storage NO
+
+//url to buy more storage
+#define k_buy_more_storage_url @"http://owncloud.com/mobile/help"
+
+//Show impressum
+#define k_show_imprint_option_on_settings YES
+
+//Show social
+#define k_show_recommend_option_on_settings NO
+
+
+//Buy more storage company name
+#define k_company_name_buy_more_storage @"freenetMail"
+
+//Customice UITabBar
+#define k_is_customize_uitabbar YES
+
+//Customize Unselected UITabBarItems (The images of tabBar should be the unseleted tabs)
+#define k_is_customize_unselectedUITabBarItems YES
+
+//Set Image on Preview Bottom toolBar on iPhone
+#define k_set_image_on_preview_bottom_toolBar_on_iPhone NO
+
+//Impressum is a File
+#define k_impressum_is_file NO
+
+//Impressum url if is not a file
+#define k_impressum_url @"http://impresium.eseseseetitolechecita"
+
+
+//Customice recomend mail
+#define k_is_custom_recommend_mail NO
+#define k_is_username_recommend_mail NO
+//#define k_subject_recommend_mail @"Sehen Sie sich die mobilcom-debitel Cloud an!"
+#define k_subject_recommend_mail @""
+#define k_text_recommend_mail @""
+#define k_is_sign_custom_usign_username NO
+
+//Customize background of bar bottom preview with image or color
+#define k_is_image_background_preview_bottom_bar NO
+
+//Social customize
+#define k_is_custom_twitter NO
+#define k_custom_twitter_message @""
+
+#define k_is_custom_facebook NO
+#define k_custom_facebook_message @""
+
+
+//Autocomplete Login
+#define k_is_autocomplete_username_necessary NO
+#define k_letter_to_begin_autocomplete @"@"
+#define k_text_to_autocomplete @"cloud.md.de"
+
+//Number of uploads shown in recents tab from the database
+#define k_number_uploads_shown 30
+//Set text of status bar white only for iOS 7. YES = White | NO = Black
+#define k_is_text_status_bar_white NO
---
+++ md/iOS/Branding/UIColor+Constants.m
@@ -0,0 +1,150 @@
+//
+//  UIColor+Constants.m
+//  ownCloud iOs Client
+//
+//  Created by OWNBRANDER
+//
+//
+
+#import "UIColor+Constants.h"
+
+@implementation UIColor(Constants)
+
+//NAVIGATION AND TOOL BAR
+
+//Tint color of navigation bar
++ (UIColor*)colorOfNavigationBar{
+    return [UIColor colorWithRed:28/255.0f green:44/255.0f blue:67/255.0f alpha:1.0];
+}
+//Color of background view in navigation bar, only for iOS 7 for transparency
++ (UIColor*)colorOfBackgroundNavBarImage {
+    return [UIColor colorWithRed:28/255.0f green:44/255.0f blue:67/255.0f alpha:0.7];
+}
+
+//Color of letters in navigation bar
++ (UIColor*)colorOfNavigationTitle{
+    return [UIColor colorWithRed:255/255.0f green:255/255.0f blue:255/255.0f alpha:1.0];
+}
+
+//Color of items in navigation bar
++ (UIColor*)colorOfNavigationItems{
+    return [UIColor colorWithRed:255/255.0f green:255/255.0f blue:255/255.0f alpha:1.0];
+}
+
+//Section index color
++ (UIColor*)colorOfSectionIndexColorFileList {
+    return [UIColor colorWithRed:28/255.0f green:44/255.0f blue:67/255.0f alpha:0.7];
+}
+
+//Tint color of tool bar
++ (UIColor*)colorOfToolBar{
+    return [UIColor colorWithRed:28/255.0f green:44/255.0f blue:67/255.0f alpha:1.0];
+}
+//Color of background view in toolBar bar, only for iOS 7 for transparency
++ (UIColor*)colorOfBackgroundToolBarImage {
+    return [UIColor colorWithRed:28/255.0f green:44/255.0f blue:67/255.0f alpha:0.7];
+}
+
+//Tint color of tool bar buttons
++ (UIColor*)colorOfToolBarButtons {
+    //To iOS 7
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 7)
+    {
+        return [UIColor colorWithRed:255/255.0f green:255/255.0f blue:255/255.0f alpha:1.0];
+
+    } else {
+        //To iOS 6
+        return [UIColor colorWithRed:255/255.0f green:255/255.0f blue:255/255.0f alpha:1.0];
+    }
+}
+
+//TAB BAR
+
+//Tint color of tab bar
++ (UIColor*)colorOfTintUITabBar {
+    return [UIColor colorWithRed:28/255.0f green:44/255.0f blue:67/255.0f alpha:1.0];
+}
+
+//Tint color fo selected tab bar item
++ (UIColor*)colorOfTintSelectedUITabBar {
+    return [UIColor colorWithRed:255/255.0f green:255/255.0f blue:255/255.0f alpha:1.0];
+}
+
+//Tint color for non selected tab bar item (only works with the labels)
++ (UIColor*)colorOfTintNonSelectedUITabBar {  
+    return [UIColor colorWithRed:0/255.0f green:0/255.0f blue:0/255.0f alpha:1.0];
+}
+
+//SETTINGS VIEW
+
+//Text color in some cells of settings view
++ (UIColor*)colorOfDetailTextSettings {
+    return [UIColor whiteColor];
+}
+
+//Cell background color in some cells of settings view
++(UIColor*)colorOfBackgroundButtonOnList {
+    return [UIColor colorWithRed:255/255.0f green:255/255.0f blue:255/255.0f alpha:1.0];
+}
+
+//Text color in some cells of settings view
++(UIColor*)colorOfTextButtonOnList {
+    return [UIColor colorWithRed:0/255.0f green:0/255.0f blue:0/255.0f alpha:1.0];
+}
+
+//LOGIN VIEW
+
+//Background color of login view
++ (UIColor*)colorOfLoginBackground{
+    return [UIColor colorWithRed:125/255.0f green:206/255.0f blue:240/255.0f alpha:1.0];
+}
+
+//Background color of top of login view, in logo image view
++ (UIColor*)colorOfLoginTopBackground {
+    return [UIColor colorWithRed:231/255.0f green:242/255.0f blue:248/255.0f alpha:1.0];
+}
+
++ (UIColor*)colorOfLoginButtonBackground {   
+    return [UIColor colorWithRed:239/255.0f green:239/255.0f blue:239/255.0f alpha:1.0];
+}
+
+//Log in button text color (UIColor+Constants.m)
++ (UIColor*)colorOfLoginButtonTextColor {
+    return [UIColor colorWithRed:0/255.0f green:0/255.0f blue:0/255.0f alpha:1.0];
+}
+
+//Text color of url in login view
++ (UIColor*)colorOfURLUserPassword{
+    return [UIColor colorWithRed:60/255.0f green:60/255.0f blue:59/255.0f alpha:1.0];
+}
+
+//Text color of login text
++ (UIColor*)colorOfLoginText {
+     return [UIColor colorWithRed:255/255.0f green:255/255.0f blue:255/255.0f alpha:1.0];
+}
+
+//Text color of error credentials
++ (UIColor*)colorOfLoginErrorText{
+    return [UIColor colorWithRed:255/255.0f green:255/255.0f blue:255/255.0f alpha:1.0];
+}
+
+//Text color of server error
++ (UIColor*)colorOfServerErrorText{
+    return [UIColor colorWithRed:255/255.0f green:255/255.0f blue:255/255.0f alpha:1.0];
+}
+
+//FILE LIST
+//Text color of selected folder
++ (UIColor*)colorOfTxtSelectFolderToolbar {
+    return [UIColor whiteColor];
+}
+
+//WEB VIEW
+//Color of webview background
++ (UIColor*)colorOfWebViewBackground{
+   return [UIColor colorWithRed:26/255 green:26/255 blue:28/255 alpha:1.0];
+}
++ (UIColor*)colorOfBackgroundDetailViewiPad{
+    return [UIColor whiteColor];
+}
+@end
